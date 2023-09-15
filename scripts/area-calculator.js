8e53c72fbb6a08186fcf483aca684ebabbbe384b
afmsabbire <khanafmsabbir@gmail.com>
2023-09-16 00:06:02 +0600

    add reduce code

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,72 +1,40 @@
 function calculateTriangleArea(){
-    // get triangle base
-    const baseField = document.getElementById("triangle-b");
-    const baseFieldText = baseField.value;
-    const base = parseFloat(baseFieldText);
-
-    // get triangle height
-    const heightField = document.getElementById("triangle-h");
-    const heightFieldText = heightField.value;
-    const height = parseFloat(heightFieldText);
+    const base = getInputField("triangle-b");
+    const height = getInputField("triangle-h");
 
     const area = 0.5 * base * height;
-    const areaSpan = document.getElementById("triangle-area");
-    areaSpan.innerText = area;
+    setElementInnerText("triangle-area", area);
 }
 
 function calculateRectangleArea() {
-    
-    const widthField = document.getElementById("rectangle-w");
-    const widthFieldText = widthField.value;
-    const width = parseFloat(widthFieldText);
-
-    const lengthField = document.getElementById("rectangle-l");
-    const lengthFieldText = lengthField.value;
-    const length = parseFloat(lengthFieldText);
+    const width = getInputField("rectangle-w");
+    const length = getInputField("rectangle-l");
 
     const area = width * length;
-    const areaSpan = document.getElementById("rectangle-area");
-    areaSpan.innerText = area;
+    setElementInnerText("rectangle-area", area);
 }
 
 function calculateParallelogramArea(){
-    const baseField = document.getElementById("parallelogram-b");
-    const baseFieldText = baseField.value;
-    const base = parseFloat(baseFieldText);
-
-    const heightField = document.getElementById("parallelogram-h");
-    const heightFieldText = heightField.value;
-    const height = parseFloat(heightFieldText);
+    const base = getInputField("parallelogram-b");
+    const height = getInputField("parallelogram-h");
 
     const area = base * height;
-    const areaSpan = document.getElementById("parallelogram-area");
-    areaSpan.innerText = area;
+    setElementInnerText("parallelogram-area", area);
 }
 
 function calculateRhombusArea(){
-    const valued1Field = document.getElementById("rhombus-d1");
-    const valued1Text = valued1Field.value;
-    const valued1 = parseFloat(valued1Text);
+    const d1 = getInputField("rhombus-d1");
+    const d2 = getInputField("rhombus-d2");
 
-    const valued2Field = document.getElementById("rhombus-d2");
-    const valued2Text = valued2Field.value;
-    const valued2 = parseFloat(valued2Text);
-
-    const area = 0.5 * valued1 * valued2;
-    const areaSpan = document.getElementById("rhombus-area");
-    areaSpan.innerText = area;
+    const area = 0.5 * d1 * d2;
+    setElementInnerText("rhombus-area", area);
 }
 
 function calculatePentagonArea(){
-    const perimeterField = document.getElementById("pentagon-p");
-    const perimeterFieldText = perimeterField.value;
-    const perimeter = parseFloat(perimeterFieldText);
-
-    const baseField = document.getElementById("pentagon-b");
-    const baseFieldText = baseField.value;
-    const base = parseFloat(baseFieldText);
+    const perimeter = getInputField("pentagon-p");
+    const base = getInputField("pentagon-b");
 
     const area = 0.5 * perimeter * base;
-    const areaSpan = document.getElementById("pentagon-area");
-    areaSpan.innerText = area;
+    setElementInnerText("pentagon-area", area);
+    
 }
---
+++ scripts/utilities.js
@@ -0,0 +1,11 @@
+function getInputField(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputFieldText = inputField.value;
+    const input = parseFloat(inputFieldText);
+    return input;
+}
+
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
