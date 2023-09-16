6a1d9d790a3c1c923db4d1f67f762fd8d62a8da2
afmsabbire <khanafmsabbir@gmail.com>
2023-09-16 23:57:22 +0600

    finished

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -2,45 +2,68 @@ function calculateTriangleArea() {
   const base = getInputField("triangle-b");
   const height = getInputField("triangle-h");
 
+  isNanValue(base, height);
+
   const area = 0.5 * base * height;
   setElementInnerText("triangle-area", area);
+  addToCalculationEntry("Triangle", area);
 }
 
 function calculateRectangleArea() {
   const width = getInputField("rectangle-w");
   const length = getInputField("rectangle-l");
 
+  isNanValue(width, length);
+
   const area = width * length;
   setElementInnerText("rectangle-area", area);
+  addToCalculationEntry("Rectangle", area);
+  
 }
 
 function calculateParallelogramArea() {
   const base = getInputField("parallelogram-b");
   const height = getInputField("parallelogram-h");
 
+  isNanValue(base, height);
+
   const area = base * height;
   setElementInnerText("parallelogram-area", area);
+  addToCalculationEntry("Parallelogram", area);
 }
 
 function calculateRhombusArea() {
   const d1 = getInputField("rhombus-d1");
   const d2 = getInputField("rhombus-d2");
 
+  isNanValue(d1, d2);
+
   const area = 0.5 * d1 * d2;
   setElementInnerText("rhombus-area", area);
+  addToCalculationEntry("Rhombus", area);
 }
 
 function calculatePentagonArea() {
   const perimeter = getInputField("pentagon-p");
   const base = getInputField("pentagon-b");
 
+  isNanValue(perimeter, base);
+
   const area = 0.5 * perimeter * base;
   setElementInnerText("pentagon-area", area);
+  addToCalculationEntry("Pentagon", area);
 }
 function calculateEllipseArea() {
   const a = getInputField("ellipse-a");
   const b = getInputField("ellipse-b");
 
+  isNanValue(a, b);
+
   const area = Math.PI * a * b;
   setElementInnerText("ellipse-area", area);
+  addToCalculationEntry("Ellipse", area);
 }
+
+// calculation Entry:
+
+
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -9,3 +9,21 @@ function setElementInnerText(elementId, area) {
   const element = document.getElementById(elementId);
   element.innerText = area;
 }
+
+// isNaN :
+function isNanValue(value1, value2) {
+  if (isNaN(value1) || isNaN(value2)) {
+    alert("please insert Number.");
+    return;
+  }
+}
+
+// add to calculation entry:
+function addToCalculationEntry(areaType, area) {
+  const calculationEntry = document.getElementById("calculation-entry");
+  const p = document.createElement("p");
+  const count = calculationEntry.childElementCount;
+  p.classList.add("my-3")
+  p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup>`;
+  calculationEntry.appendChild(p);
+}
